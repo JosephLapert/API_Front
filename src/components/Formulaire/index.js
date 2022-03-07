5b7660d9c945b6f8dd80901002b69e3f8598086f
Naeylin <yannvallet2097@gmail.com>
2022-03-07 15:50:40 +0100

    ajout des id pour le css

--- src/components/Formulaire/index.js
+++ src/components/Formulaire/index.js
@@ -15,33 +15,38 @@ const NameForm = () => {
     }
     const formRef = useRef();
     return (
-        <form ref={formRef} onSubmit={(e) => handleSubmit(e)}>
-            <label>
-                Nom: 
-                <input type="text" name="nom"/> <br />
-                Prenom: 
-                <input type="text" name="prenom"/> <br />
-                Date de naissance:
-                <input type="date" name="ddn"/> <br />
-                Sexe:
-                <input type="text" name="sexe"/> <br />
-                Adresse:
-                <input type="text" name="adresse"/> <br />
-                Code postal:
-                <input type="text" name="cp"/> <br />
-                Ville:
-                <input type="text" name="ville"/> <br />
-                Pays:
-                <input type="text" name="pays"/> <br />
-                Mobile:
-                <input type="text" name="mobile"/> <br />
-                Email:
-                <input type="text" name="email"/> <br />
-                Password:
-                <input type="password" name="psswd"/> <br />
-            </label>
-                <input type="submit" value="Submit" />            
-        </form>
+        <div id='div_formulaire'>
+            <h3 id='titre_formulaire_css'>
+                Formulaire d'inscription
+            </h3>
+            <form id='formulaire_css' ref={formRef} onSubmit={(e) => handleSubmit(e)}>
+                <label>
+                    Nom: 
+                    <input id='formulaire_saisie_css' type="text" name="nom"/> <br />
+                    Prenom: 
+                    <input id='formulaire_saisie_css' type="text" name="prenom"/> <br />
+                    Date de naissance:
+                    <input id='formulaire_saisie_css' type="date" name="ddn"/> <br />
+                    Sexe:
+                    <input id='formulaire_saisie_css' type="text" name="sexe"/> <br />
+                    Adresse:
+                    <input id='formulaire_saisie_css' type="text" name="adresse"/> <br />
+                    Code postal:
+                    <input id='formulaire_saisie_css' type="text" name="cp"/> <br />
+                    Ville:
+                    <input id='formulaire_saisie_css' type="text" name="ville"/> <br />
+                    Pays:
+                    <input id='formulaire_saisie_css' type="text" name="pays"/> <br />
+                    Mobile:
+                    <input id='formulaire_saisie_css' type="text" name="mobile"/> <br />
+                    Email:
+                    <input id='formulaire_saisie_css' type="text" name="email"/> <br />
+                    Password:
+                    <input id='formulaire_saisie_css' type="password" name="psswd"/> <br />
+                </label>
+                    <input id='button_form_css' type="submit" value="Submit" />            
+            </form>
+        </div>
     );
 };
 export default NameForm;
