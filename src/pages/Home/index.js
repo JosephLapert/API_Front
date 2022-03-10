05b300b51cd7f0d51e0ed119a9367343cd827833
JosephLapert <JosephLapert@gmail.com>
2022-03-10 15:07:51 +0100

    création du composant log dans home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import axios from 'axios';
 
 const Home = () => {
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const form ={};
+        const inputs = e.target.querySelectorAll('.form-input');
+        Array.from(inputs).forEach(input => {
+            form[input.name] = input.value
+        })
+        try {
+            await axios.post('http://localhost:8000/api/auth/', form)
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div>
             <h3>
-                Bienvenue sur la page d'accueil de Maison des Ligues.
+                Bienvenue sur la page d'accueil de Maison des Ligues.        
             </h3>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <label>
+                    Email:
+                    <input className="form-input" type="text" name="email" />
+                    Password:
+                    <input className="form-input" type="password" name="psswd" />
+                </label> 
+                    <input type="submit" value="submit" />
+            </form>
         </div>
     );
 };
 
-export default Home;
+export default Home;    
