3f5c01789ae4ce5e746120e58cf7ad167960202d
Naeylin <yannvallet2097@gmail.com>
2022-03-14 13:51:47 +0100

    changement lie a un log

--- src/pages/ListTodo/index.js
+++ src/pages/ListTodo/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const ListUsers = () => {
 
     const [todos, setTodos] = React.useState( [] );
 
     React.useEffect(() => {
-        axios.get('http://localhost:8000/api/test')
+        axios.get('http://localhost:8000/api/users/')
             .then((response) => {
-            setTodos(response.data.success);
+            setTodos(response.data.success[0]);
             // console.log(response);
         })
     }, []);
@@ -17,16 +17,17 @@ const ListUsers = () => {
     if (todos.length === 0 ) {
         return null;
     }
-
+  
     // console.log(todos);
 
     return (
         <div id='div_list_todos'>
             <h3 id='titre_list_todos_css'>Liste des inscrits</h3>
             {
-                todos.map(todo => (
-                    <div id='list_todos_css' key={todo.id}>{ todo.nom } { todo.prenom } { todo.ddn }</div> 
-                ))
+                todos.map(todo => {
+                    console.log(todo)
+                    return <div id='list_todos_css' key={todo.id_utilisateur}>{ todo.nom } { todo.prenom } { todo.ddn }</div> 
+                })
             
             }
 
