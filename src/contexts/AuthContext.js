a79c398f47afa844f9bd2afd377e8d0cc933e6f4
Lapertjoseph <lapertjoseph@gmail.com>
2022-03-26 16:00:37 +0100

    fixed typo navigate dans authcontext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -35,7 +35,7 @@ const AuthContextProvider = ({children}) => {
         try {
            const result =  await axios.post('/auth', form)
             setUser(result.data.success)
-            navigate('/seances')            
+            navigate('/seance')            
         } catch (error) {
             console.log(error);
             navigate('/')
