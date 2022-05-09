8afeeaa92ef2817ecfe58c5d8ee6d59e4d943417
Lapertjoseph <lapertjoseph@gmail.com>
2022-05-09 16:32:42 +0200

    create page protected

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Outlet, Navigate } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 import Formulaire from "./components/Formulaire";
 import SeanceCoach from "./components/SeanceCoach";
 import UpdateUtilisateur from "./components/UpdateUtilisateur";
-import Inscription from "./components/Inscription";
-
-import './App.css'
 import Logout from "./components/Logout";
 
+import { useAuth } from "./contexts/AuthContext";
+
+import "./App.css";
+
+const ProtectedRoute = () => {
+  const { user } = useAuth();
+  return user ? <Outlet /> : <Navigate to="/" />;
+};
+
 function App() {
+  const { user } = useAuth();
+
   return (
     <div className="App">
-      <nav>
+      {user && <nav>
         <Link to="/">Home</Link>
         <Link to="/form">Inscription</Link>
         <Link to="/seance">Date de séance</Link>
         <Link to="/update">Modification de profil</Link>
         <Logout />
-      </nav>
+      </nav>}
       <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/form" element={<Formulaire />} />
-        <Route path="/seance" element={<SeanceCoach />} />
-        <Route path="/update" element={<UpdateUtilisateur />} />
-        <Route path="/test" element={<Inscription />} />
+          <Route path="/" element={<Home />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/form" element={<Formulaire />} />
+          <Route path="/seance" element={<SeanceCoach />} />
+          <Route path="/update" element={<UpdateUtilisateur />} />
+        </Route>
       </Routes>
     </div>
   );
